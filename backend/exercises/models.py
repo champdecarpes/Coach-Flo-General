a6a22201778e3c12e6cc1ad372bfd57c9d937cea
champdecarpes <nikita.polikarpov050@gmail.cpm>
2025-07-19 20:28:27 +0300

    0.0.4 - Upd: exercise model

--- backend/exercises/models.py
+++ backend/exercises/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
 
 class Exercise(models.Model):
-    modality = models.CharField(max_length=50, choices=[
+    modality = models.TextField(choices=[
         ('activation', 'Activation'),
         ('agility', 'Agility'),
         ('cardio', 'Cardio'),
@@ -12,8 +14,7 @@ class Exercise(models.Model):
         ('strength', 'Strength'),
         ('yoga', 'Yoga')
     ])
-    primary_focus = models.CharField(max_length=50)
-    muscle_group = models.CharField(max_length=50, choices=[
+    muscle_group = models.TextField(choices=[
         ('biceps', 'Biceps'),
         ('chest', 'Chest'),
         ('core', 'Core'),
@@ -32,7 +33,7 @@ class Exercise(models.Model):
         ('upper_back_and_neck', 'Upper Back & Neck'),
         ('upper_body', 'Upper Body')
     ])
-    movement_pattern = models.CharField(max_length=50, choices=[
+    movement_pattern = models.TextField(choices=[
         ('carry_gait', 'Carry / Gait'),
         ('core_bracing', 'Core Bracing'),
         ('core_flexion_extension', 'Core Flexion / Extension'),
@@ -45,32 +46,41 @@ class Exercise(models.Model):
         ('upper_body_vertical_pull', 'Upper Body Vertical Pull'),
         ('upper_body_vertical_push', 'Upper Body Vertical Push')
     ])
-    strength = models.CharField(max_length=50)
-    bodyweight = models.CharField(max_length=50)
-    timed = models.CharField(max_length=50)
-    distance_x_time = models.CharField(max_length=50)
-    category = models.CharField(max_length=50)
-    instructions = models.TextField()
-    links = models.TextField()
-    advanced_settings = models.TextField()
-    tracking_fields = models.ManyToManyField('TrackingFields', limit_choices_to={'id__lte': 3})
+    strength = models.DecimalField(max_length=5, decimal_places=2, default=0)
+    bodyweight = models.DecimalField(max_length=5, decimal_places=2, default=0)
+    timed = models.TimeField(default=0)
+    distance_x_time = models.DecimalField(max_length=50, default=0.0, decimal_places=2)
+    instructions = models.TextField(max_length=500, default='')
+    links = models.TextField(max_length=500, default='')
+    default_note = models.TextField(max_length=500, default='')
+
+    # Все возможные поля для мониторинга
+    tracking_fields = models.OneToOneField('TrackingFields',on_delete=models.CASCADE)
+
+    # Только названия полей, которые мониторятся
+    monitored_fields = models.JSONField(default=list, blank=True)
+
+    def clean(self):
+        # Проверка, что количество названий полей не превышает 3
+        if self.monitored_fields and len(self.monitored_fields) > 3:
+            raise ValidationError("Можно мониторить не более 3 полей.")
 
     class Meta:
         verbose_name = "Exercise"
         verbose_name_plural = "Exercises"
 
 class TrackingFields(models.Model):
-    time = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.TimeField(max_digits=10, decimal_places=2)
     speed = models.DecimalField(max_digits=10, decimal_places=2)
     cadence = models.DecimalField(max_digits=5, decimal_places=3)
     distance = models.DecimalField(max_digits=10, decimal_places=2)
-    reps = models.IntegerField(max_digits=10, decimal_places=2)
+    reps = models.IntegerField(default=0)
     weight = models.DecimalField(max_digits=10, decimal_places=2)
     heart_rate = models.DecimalField(max_digits=10, decimal_places=2)
     percentage_hr = models.DecimalField(max_digits=4, decimal_places=3)
     rpm = models.DecimalField(max_digits=10, decimal_places=2)
-    round_field = models.IntegerField(max_digits=10, decimal_places=2)
-    rest = models.DecimalField(max_digits=10, decimal_places=2)
+    round_field = models.IntegerField()
+    rest = models.TimeField(max_digits=10, decimal_places=2)
 
     class Meta:
         verbose_name = "Tracking Field"
